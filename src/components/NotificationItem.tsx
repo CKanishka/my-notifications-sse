e3a48c934b76ab56c7d9c2d85f54ec0cc66f1d1e
CKanishka <ckanishka24@gmail.com>
2024-04-06 02:20:11 +0530

    show unread indication

--- src/components/NotificationItem.tsx
+++ src/components/NotificationItem.tsx
@@ -1,28 +1,23 @@
+import { useState } from "react";
+
 const NotificationItem: React.FC<{ title: string; info: string }> = ({
   title,
   info,
 }) => {
+  const [read, setRead] = useState(false);
+
   return (
-    <div className="-mx-2 flex items-start space-x-4 rounded-md p-2 transition-all hover:bg-accent hover:text-accent-foreground / first:mt-3">
-      <svg
-        width="15"
-        height="15"
-        viewBox="0 0 15 15"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        className="mr-1 fill-sky-400 text-sky-400"
-      >
-        <path
-          d="M0.877075 7.49991C0.877075 3.84222 3.84222 0.877075 7.49991 0.877075C11.1576 0.877075 14.1227 3.84222 14.1227 7.49991C14.1227 11.1576 11.1576 14.1227 7.49991 14.1227C3.84222 14.1227 0.877075 11.1576 0.877075 7.49991ZM7.49991 1.82708C4.36689 1.82708 1.82708 4.36689 1.82708 7.49991C1.82708 10.6329 4.36689 13.1727 7.49991 13.1727C10.6329 13.1727 13.1727 10.6329 13.1727 7.49991C13.1727 4.36689 10.6329 1.82708 7.49991 1.82708Z"
-          fill="currentColor"
-          fill-rule="evenodd"
-          clip-rule="evenodd"
-        ></path>
-      </svg>
+    <div
+      className="-mx-2 flex items-start space-x-4 rounded-md p-2 transition-all hover:bg-zinc-800 hover:text-accent-foreground / first:mt-3 / cursor-pointer / relative"
+      onClick={() => setRead(true)}
+    >
       <div className="space-y-1">
         <p className="text-sm font-medium leading-none">{title}</p>
         <p className="text-sm text-muted-foreground">{info}</p>
       </div>
+      {!read && (
+        <div className="w-2 h-2 / rounded-full / bg-blue-700 / absolute right-0 top-2.5" />
+      )}
     </div>
   );
 };
